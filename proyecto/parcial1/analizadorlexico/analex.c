1385f93515c9ecbd6d897b5aa5969578cd4f367f
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2018-09-13 05:47:08 -0500

    funicona pero se necesita limpiar buffer

--- proyecto/parcial1/analizadorlexico/analex.c
+++ proyecto/parcial1/analizadorlexico/analex.c
@@ -17,26 +17,161 @@
 							fprintf(stderr, __VA_ARGS__ );			\
 							}while(0)
 #endif
+#define SIZEBUFF 100
+#define SIZEARRAY 10000
+
+typedef struct tok{
+  int line;
+  int col;
+  char *tag;
+  char *value;
+}token;
+
+/*table of symbols*/
+/*0->\t,1->\n,2->space,3-> \r*/
+/*4->{,5->},6->(,7->)*/
+/*8->,,9->;,10-> <,11-> >*/
+/*12-> =,13-> !,14 -> |,15-> &*/
+static char syms[]   ={'\t' , '\n' , ' ' , '\r',\
+                              '{' , '}' , '(' , ')' ,\
+                              ',' , ';' , '<' , '>' ,\
+                              '=' , '!' , '|' , '&' \
+                            };
+static char *tags[] ={\
+                        "ID","INT","REAL","PAR", \
+                        "COR","COMA","OR","PC", \
+                        "OA","OR","NEG","OP"\
+                      };
+
+
+
+char compare2syms( char data);
+
+char search ( char data)
+{
+  /*100-number
+   *101letter
+   *255-error
+   *or number of symbol in the table
+  */
+  char data2return = 255;
+
+  if(isdigit(data))
+  {
+    //printf("digito\n" );
+    data2return = 100;
+  }
+  else if(isupper(data) | islower(data))
+  {
+    //printf("letra\n" );
+    data2return = 101;
+  }
+  else{
+  data2return = compare2syms(data);
+  //printf("simbolo numero :%i\n", data2return );
+  }
+  return data2return;
+}
+
+char compare2syms( char data)
+{
+  /*
+   * 255 - flase
+   *
+   */
+  int count;
+  char *ptrsyms;
+  char data2return;
+  ptrsyms = syms;
+  count=16;
+  data2return=255;
+
+  while ((count--) > 0)
+  {
+    if (data == *ptrsyms)
+    {
+      data2return = count-1;
+      break;
+    }
+  }
+  return data2return;
+}
 
 
 int main (int argc, char **argv)
 {
-  const char syms[]   ={'\t','\n', ' ', '\r' };   /*table of symbols*/
-  char * readFileName = argv[1];                  /*path of read file*/
-  FILE * readFile     = fopen(readFileName,"r");  /*pointer to read file*/
-  char character;
 
+  char *readFileName;                            /*path of read file*/
+  FILE *readFile;                                /*pointer to read file*/
+  char character;
+  char buffer[SIZEBUFF];
+  char *ptrbuffer;
+  char *ptrbufferstart;
+  int numline;
+  int numcol;
+  token arrayTokens[SIZEARRAY];
+  int numoftokens;
+
+  readFileName = argv[1];
+  readFile     = fopen(readFileName,"r");
+  ptrbuffer= buffer;
+  ptrbufferstart= buffer;
+  numcol=0;
+  numline=0;
   character = 0;
+  numoftokens=0;
+
+  #define SAVENEWTOKEN(X,Y) do{\
+                            arrayTokens[numoftokens].line=numline;\
+                            arrayTokens[numoftokens].col=numcol;\
+                            arrayTokens[numoftokens].tag=(X);\
+                            arrayTokens[numoftokens].value=(Y);\
+                            ptrbuffer=ptrbufferstart;\
+                            
+                            }while(0);
+
   PRINTDEBUG("Open File in path: %s\n", readFileName);
   while((character = fgetc(readFile)) != EOF)
   {
-    putchar( character );
+    //#ifdef TEST2
+    //putchar( character );
+    //#endif
+    if (islower(character)) //id
+    {
+
+      *ptrbuffer = character;
+      ptrbuffer++;
+      numcol++,
+      character = fgetc(readFile);
+
+      while(search(character) == 100 | search(character) == 101)
+      {
+        //printf("%s %s: \n" , "entro a ciclo: ", buffer);
+        *ptrbuffer = character;
+        ptrbuffer++;
+        numcol++;
+        character = fgetc(readFile);
+      }
+      if( (search(character) != 255 ) | (search(character) != 4) | \
+          (search(character) != 5)   | (search(character) != 7)  | \
+          (search(character) != 13) )
+      {
+        printf("%s %s\n", "termine cadena: ", buffer );
+        SAVENEWTOKEN((&arrayTokens[0]),ptrbufferstart);
+
+
+      }
+
+    }
+
+
 
   }
   PRINTDEBUG("Cerrando archivo: %s\n", readFileName);
   fclose(readFile);
   return 0;
 }
+
 
 
 
