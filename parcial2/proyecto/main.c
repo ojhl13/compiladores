956c39a4c6b0f4d64eb507dc2b05bcf8608fff0e
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2018-10-25 11:41:23 -0500

    version final sin saber hasta donde funciona

--- parcial2/proyecto/main.c
+++ parcial2/proyecto/main.c
@@ -12,6 +12,13 @@
 
 
 
+typedef struct {
+  char operador[SIZEWORD];
+  char openado1[SIZEWORD];
+  char operando2[SIZEWORD];
+}tripleta;
+
+tripleta codeinter[100];
 
 char operators[] = "+-*/%=";
 char arrayofreadline [SIZEARRAY][SIZEWORD];
@@ -20,7 +27,7 @@ int indexWord =0;
 int temporales=0;
 int parametros=0;
 int labels=0;
-
+int indextripletas
 void Analize(int numberofwords)
 {
   int indexArraytemp=0;
@@ -35,19 +42,167 @@ void Analize(int numberofwords)
   //printf("%s\n", temp );
   if( strcmp(temp,"int")==0)
   {
+    //printf("%s\n",  "tengo un int");
+    indexArraytemp+=2;
+    temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+    if(*temp == '(')//definicion de funcion
+    {
+      temp = ((char*)&arrayofreadline[indexArraytemp-2][0]);
+      printf("%s %s\n", "entry",temp );
+      codeinter[indextripletas].operador="entry";
+      codeinter[indextripletas]..openado1=temp;
+      indextripletas++;
+    }
+    else if( *temp == '=')
+    {
+      indexArraytemp+=1;
+      temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+        if( isdigit(*temp))
+        {
+          temp = ((char*)&arrayofreadline[indexArraytemp-2][0]);
+          printf("%s = ",temp );
+          codeinter[indextripletas]..openado1=temp;
+          temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+          printf("%s\n", temp);
+          codeinter[indextripletas].operador="=";
+          codeinter[indextripletas]..openado2=temp;
+          indextripletas++;
+        }
+    }
+
   }
   else if ( strcmp(temp,"void")==0)
   {
+    //printf("%s\n",  "tengo un void");
+    indexArraytemp+=2;
+    temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+    if(*temp == '(')//definicion de funcion
+    {
+      temp = ((char*)&arrayofreadline[indexArraytemp-2][0]);
+      printf("%s %s\n", "entry",temp );
+      codeinter[indextripletas].operador="entry";
+      codeinter[indextripletas]..openado1=temp;
+      indextripletas++;
+
+    }
+  }
+  else if( strcmp(temp,"if")==0 )
+  {
+    //printf("%s\n",  "tengo un if");
+    indexArraytemp+=3;
+    temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+    if(*temp == ')')//definicion de funcion
+    {
+      temp = ((char*)&arrayofreadline[indexArraytemp-1][0]);
+      printf("if flase %s goto L%i\n", temp ,labels);
+      labels++;
+      codeinter[indextripletas].operador="if";
+      codeinter[indextripletas]..openado1=temp;
+      codeinter[indextripletas]..openado2=("L%i",labels);
+      indextripletas++;
+    }
+    else{
+      temp = ((char*)&arrayofreadline[indexArraytemp-1][0]);
+      printf("t%i = %s", temporales,temp);
+      temporales++;
+      temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+      printf("%s", temp);
+      temp = ((char*)&arrayofreadline[indexArraytemp+1][0]);
+      printf("%s\n", temp);
+      temp = ((char*)&arrayofreadline[indexArraytemp+1][0]);
+      printf("if false t%i goto L%i\n",temporales,labels );
+      labels++;
+    }
   }
   else if (strcmp(temp,"else")==0) {
+    //printf("%s\n",  "tengo un else");
+    printf("LABEL L%i\n", (labels-1));
   }
   else if (strcmp(temp,"return")==0) {
+
+    //printf("%s\n",  "tengo un return");
+    indexArraytemp+=1;
+    temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+    if(isdigit(*temp))
+    {
+      printf("return %s\n", temp);
+    }
+    else if( isalpha(*temp))
+    {
+      indexArraytemp+=1;
+      temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+      for(int i = 0; i < 6; ++i){
+          if(*temp= operators[i]){
+            printf("t%i =",temporales );
+            temp = ((char*)&arrayofreadline[indexArraytemp-1][0]);
+            printf("%s",temp );
+            temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+            printf("%s",temp );
+            temp = ((char*)&arrayofreadline[indexArraytemp+1][0]);
+            printf("%s\n",temp );
+            printf("return t%i\n",temporales );
+          }
+        }
+
+    }
   }
   else if (strcmp(temp,"while")==0) {
     //printf("%s\n",  "tengo un return");
   }
   else // variable con operacion o llamada a funcion
   {
+    indexArraytemp++;
+    temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+    if(*temp == '(')//lamado a funcion
+    {
+      FUNCION:
+        if(isalpha(*temp)){
+          printf("%s\n", "Begin_args");
+          indexArraytemp++;
+          temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+          for(int i = 0; i < 6; ++i){
+              if(*temp == operators[i]){
+                  printf("t%i =",temporales );
+                  temp = ((char*)&arrayofreadline[indexArraytemp-1][0]);
+                  printf("%s",temp );
+                  temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+                  printf("%s",temp );
+                  temp = ((char*)&arrayofreadline[indexArraytemp+1][0]);
+                  printf("%s\n",temp );
+                }
+          }
+          printf("params t%i\n",temporales );
+          parametros++;
+          temporales++;
+          indexArraytemp++;
+          temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+          if(*temp = ')' ){
+            printf("t%i = call",temporales );
+            temp = ((char*)&arrayofreadline[indexArraytemp-3][0]);
+            printf("%s , %i",temp,parametros );
+          }
+
+        }
+    }
+    else if (*temp == '=')
+    {
+      indexArraytemp++;
+      temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+      if (isalpha(*temp))
+      {
+        indexArraytemp++;
+        temp = ((char*)&arrayofreadline[indexArraytemp][0]);
+        if(*temp == '(')
+        {
+          goto FUNCION;
+        }
+      }
+      if( strcmp(temp,"read"))
+      {
+        temp = ((char*)&arrayofreadline[indexArraytemp-2][0]);
+        printf("read %s\n", temp);
+      }
+    }
   }
 }
 
