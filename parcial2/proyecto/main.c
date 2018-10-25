fa5069ff9e77f11f9bbd3823f587bcfc434dfa81
Oscar Javier Hinojosa Luna <oscar_jl@hotmail.com>
2018-10-25 09:38:56 -0500

    separa palabras y falta el analizar

--- parcial2/proyecto/main.c
+++ parcial2/proyecto/main.c
@@ -3,20 +3,36 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 
-#define SIZEARRAY 50
+#define SIZEARRAY 100
 #define SIZEWORD 20
 #define SIZEINPUT 100
+#define SIZE      50000
 
 
+
+
+char operators[] = "+-*/%=";
+char arrayofreadline [SIZEARRAY][SIZEWORD];
+int indexArray=0;
+int indexWord =0;
+int temporales=0;
+int parametros=0;
+int labels=0;
+
+void Analize(int numberofwords)
+{
+
+}
 
 
 int main(int argc, char  **argv[]) {
-  char arrayofreadline [SIZEARRAY][SIZEWORD];
+  char Arraydoc[SIZE];
+  char *Array= Arraydoc;
+
   char *fileName = "test.txt";
   char character;
-  int indexArray=0;
-  int indexWord =0;
 
 
   char flag = 0; //1 caracter pasado esapcio 0 caracter pasado otra cosa
@@ -31,7 +47,6 @@ int main(int argc, char  **argv[]) {
 
   while((character = fgetc(myFile)) != EOF)
 	{
-  //while (character != ';') {
     /* code */
     if(isspace(character))//espacios detectados
     {
@@ -43,33 +58,62 @@ int main(int argc, char  **argv[]) {
     {
       if( flag == 0)
       {
-        putchar(character);
-
+        *Array =(character);
+        Array++;
         start =1;
       }
       else if ( flag == 1 && start == 0 )
       {
         flag=0;
         start ==1;
-        putchar(character);
-
+        *Array =(character);
+        Array++;
       }
       else
       {
           flag=0;
         start=1;
-        putchar(' ');
-        putchar(character);
-
+        *Array =(' ');
+        Array++;
+        *Array =(character);
+        Array++;
       }
     }
-      //  character = fgetc(myFile);
-//      }
-      indexWord=0;
-      indexArray=0;
-}
-
-  	printf("Cerrando archivo: %s\n", myFile);
+  }
+  *Array=EOF;
+//printarray(Arraydoc,SIZE);
+  	printf("Cerrando archivo: %s\n", fileName);
   	fclose(myFile);
+////printf("%s\n", Arraydoc );
+Array = Arraydoc;
+while(*Array != EOF)
+{
+      //buscar una linea
+      if(*Array == ';' |( *Array == '{'| *Array == '}')) {
+        Analize(indexArray);
+        printf("%s\n", "fin de linea");
+        indexWord  = 0;
+        indexArray = 0;
+      }
+      else if( *Array == ' '){
+          arrayofreadline[indexArray][indexWord]=EOF;
+          putchar(' ');
+          indexWord = 0;
+        //  //printf("%s <- palabra %i \n",(char*)&arrayofreadline[indexArray][indexWord],indexArray );
+          indexArray++;
+
+      }
+
+      else
+      {
+        arrayofreadline[indexArray][indexWord]=  *Array;
+        putchar(arrayofreadline[indexArray][indexWord]);//putchar(*Array);
+        indexWord++;
+      }
+
+  Array++;
+  }
+  printf("%s\n", "termine" );
+
   	return 0;
 }
